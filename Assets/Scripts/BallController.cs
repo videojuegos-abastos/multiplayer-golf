ad1b25bdaa96aa7320806c0896799c250c0359a7
guilleventure <guillermo@venturelabs.es>
2023-01-30 21:55:40 +0100

    ...

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,14 +4,56 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
+	[SerializeField]
+	[Tooltip("Grados de giro por segundo")]
+	float rotationDegrees;
+
+	[SerializeField]
+	float kickForce;
+
+	[SerializeField]
+	float velocityThreshold;
+
+	Transform direction;
     void Start()
     {
-        
+		// No es la forma!!
+        direction = transform.GetChild(0);
     }
 
     void Update()
     {
-        
+        direction.localRotation *= Quaternion.Euler(0, 0, -rotationDegrees * Time.deltaTime);
+
+		ManageInput();
     }
+
+	void Kick()
+	{
+
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+		bool correctVelocity = rb.velocity.magnitude < velocityThreshold;
+
+		List<ContactPoint2D> contacts = new List<ContactPoint2D>(1);
+		int contactCount = rb.GetContacts(contacts);
+	
+		bool inGround = contacts.Count > 0;
+
+		if (correctVelocity && inGround)
+		{
+			rb.AddForce(direction.up * kickForce, ForceMode2D.Impulse);
+		}
+	}
+
+	void ManageInput()
+	{
+		bool space = Input.GetKey(KeyCode.Space);
+
+		if (space)
+		{
+			Kick();
+		}
+
+	}
 }
